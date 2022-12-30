a96b516a15c282e84a0c2ceccedfe2e72ea95726
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-12-31 01:05:34 +0200

    added counter tests

---
+++ src/test/java/com/example/myTestApp/service/NumOfDoublesCounterTest.java
@@ -0,0 +1,26 @@
+package com.example.myTestApp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumOfDoublesCounterTest {
+    final NumOfDoublesCounter counter = new NumOfDoublesCounterImpl();
+
+    @Test
+    void count() {
+        var str = "";
+        var res = counter.count(str);
+        assertEquals(res, 0);
+
+        str = "fvfvf3vffvf1.4ddf0";
+        res = counter.count(str);
+        assertEquals(res, 3);
+
+        str = "2+ .2";
+        res = counter.count(str);
+        assertEquals(res, 2);
+
+    }
+}
