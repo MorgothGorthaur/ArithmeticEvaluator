3a9431b2b4793629e06df735732e8139a959f0c2
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-12-31 12:27:37 +0200

    removed unused imports

--- src/main/java/com/example/myTestApp/service/StringExpressionHandlerImpl.java
+++ src/main/java/com/example/myTestApp/service/StringExpressionHandlerImpl.java
@@ -5,14 +5,8 @@ import com.example.myTestApp.exception.MissedLeftBracketException;
 import com.example.myTestApp.exception.MissedRightBracketException;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedList;
-import java.util.List;
-
 @Component
-class StringExpressionHandlerImpl implements StringExpressionHandler{
-
-
-
+class StringExpressionHandlerImpl implements StringExpressionHandler {
     @Override
     public String[] splitByBrackets(String expression) {
         var bracketIndexes = getBracketIndexes(expression);
@@ -29,7 +23,7 @@ class StringExpressionHandlerImpl implements StringExpressionHandler{
     public double getOperand(String operation, int startIndex, int endIndex) {
         if (operation.contains("(")) throw new MissedRightBracketException();
         if (operation.contains(")")) throw new MissedLeftBracketException();
-        if(operation.equals("")) return 0;
+        if (operation.equals("")) return 0;
         try {
             return Double.parseDouble(operation.substring(startIndex, endIndex));
         } catch (RuntimeException ex) {
@@ -39,7 +33,7 @@ class StringExpressionHandlerImpl implements StringExpressionHandler{
 
     @Override
     public String removeDoubleSymbolsAndSpaces(String string) {
-        if (string.length() >0 && string.charAt(0) == '+') string = string.substring(1);
+        if (string.length() > 0 && string.charAt(0) == '+') string = string.substring(1);
         return string.strip().replaceAll("\\ ", "")
                 .replaceAll("--", "+")
                 .replaceAll("\\+-", "-")
@@ -50,25 +44,26 @@ class StringExpressionHandlerImpl implements StringExpressionHandler{
     @Override
     public int getOperandEndIndex(String operation, int opIndex) {
         var lastIndex = opIndex + 1;
-        while (lastIndex+1  < operation.length() && (operation.charAt(lastIndex + 1) == '.' || Character.isDigit(operation.charAt(lastIndex + 1)))) {
+        while (lastIndex + 1 < operation.length() && (operation.charAt(lastIndex + 1) == '.' || Character.isDigit(operation.charAt(lastIndex + 1)))) {
             lastIndex++;
         }
-        return lastIndex+1;
+        return lastIndex + 1;
     }
 
     @Override
     public int getOperandStartIndex(String operation, int opIndex) {
         var firstIndex = opIndex - 1;
-        while (firstIndex > 0  && (operation.charAt(firstIndex - 1) == '.' || Character.isDigit(operation.charAt(firstIndex - 1)))) {
+        while (firstIndex > 0 && (operation.charAt(firstIndex - 1) == '.' || Character.isDigit(operation.charAt(firstIndex - 1)))) {
             firstIndex--;
         }
-        return firstIndex > 0 && operation.charAt(firstIndex-1) == '-' ? firstIndex-1 : firstIndex;
+        return firstIndex > 0 && operation.charAt(firstIndex - 1) == '-' ? firstIndex - 1 : firstIndex;
     }
 
     @Override
     public boolean checkIfExpressionContainsBrackets(String exp) {
         return exp.contains("(") && exp.contains(")");
     }
+
     private int[] getBracketIndexes(String expression) {
         var firstBracketIndex = expression.indexOf("(");
         var lastBracketIndex = firstBracketIndex + 1;
@@ -82,6 +77,7 @@ class StringExpressionHandlerImpl implements StringExpressionHandler{
         checkIfRightBracketIsMissed(expression, lastBracketIndex);
         return new int[]{firstBracketIndex, lastBracketIndex};
     }
+
     private void checkIfRightBracketIsMissed(String expression, int lastBracketIndex) {
         if (expression.charAt(lastBracketIndex) != ')') {
             throw new MissedRightBracketException();
