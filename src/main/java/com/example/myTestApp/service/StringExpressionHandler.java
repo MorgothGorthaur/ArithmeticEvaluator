05ea9dbd0df33f15b4d00816143867d9fd6fce91
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-12-31 12:27:51 +0200

    removed unused imports

--- src/main/java/com/example/myTestApp/service/StringExpressionHandler.java
+++ src/main/java/com/example/myTestApp/service/StringExpressionHandler.java
@@ -1,12 +1,16 @@
 package com.example.myTestApp.service;
 
-import java.util.List;
 
 public interface StringExpressionHandler {
-    String [] splitByBrackets(String expression);
+    String[] splitByBrackets(String expression);
+
     double getOperand(String operation, int startIndex, int endIndex);
+
     String removeDoubleSymbolsAndSpaces(String string);
+
     int getOperandEndIndex(String operation, int opIndex);
+
     int getOperandStartIndex(String operation, int opIndex);
+
     boolean checkIfExpressionContainsBrackets(String exp);
 }
