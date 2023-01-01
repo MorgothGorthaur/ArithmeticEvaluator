9a3071c36eb473e0c52902f566113446c402fc1e
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-01 14:14:44 +0200

    transformed ApiError to record

--- src/main/java/com/example/myTestApp/model/ApiError.java
+++ src/main/java/com/example/myTestApp/model/ApiError.java
@@ -1,28 +1,3 @@
 package com.example.myTestApp.model;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ApiError {
-    private String message;
-    private String debugMessage;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private List<String> errors;
-
-    public ApiError(String message, String debugMessage) {
-        this.message = message;
-        this.debugMessage = debugMessage;
-        errors = new ArrayList<>();
-        errors.add(message);
-        errors.add(debugMessage);
-    }
+public record ApiError(String message, String debugMessage) {
 }
