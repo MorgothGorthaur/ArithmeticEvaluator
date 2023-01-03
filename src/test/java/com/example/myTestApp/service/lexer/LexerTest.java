03ec230a159d2cb60997e59e3762612133179928
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-04 01:14:14 +0200

    added new tests

--- src/test/java/com/example/myTestApp/service/lexer/LexerTest.java
+++ src/test/java/com/example/myTestApp/service/lexer/LexerTest.java
@@ -1,16 +1,18 @@
 package com.example.myTestApp.service.lexer;
 
+import com.example.myTestApp.exception.BadOperandException;
 import com.example.myTestApp.service.lexer.Lexer;
 import com.example.myTestApp.service.lexer.LexerImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LexerTest {
     private final Lexer lexer = new LexerImpl();
 
     @Test
-    void toLexemeList() {
+    void tokenize() {
         var expression = "2.5+44/(2--2)+ 3.5";
         var res = lexer.tokenize(expression);
         assertEquals(res.toString(), "[2.5, ADDITION, 44.0, DIVISION, (, 2.0, SUBTRACTION, -2.0, ), ADDITION, 3.5]");
@@ -22,5 +24,17 @@ class LexerTest {
         expression = "2++2 -+2+-2";
         res = lexer.tokenize(expression);
         assertEquals(res.toString(), "[2.0, ADDITION, 2.0, SUBTRACTION, 2.0, ADDITION, -2.0]");
+    }
+
+    @Test
+    void tokenize_shouldThrowBadOperandException() {
+        var expression = "3p4";
+        assertThrows(BadOperandException.class, () -> {
+           lexer.tokenize(expression);
+        });
+        var secondExpression = "3+p";
+        assertThrows(BadOperandException.class, () -> {
+            lexer.tokenize(secondExpression);
+        });
     }
 }
