4f8e71700965c5843cc9583720e15356a2db2e1a
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-03 02:56:06 +0200

    refactored customEvaluateTokens

--- src/main/java/com/example/myTestApp/service/updatedevaluator/parser/ParserImpl.java
+++ src/main/java/com/example/myTestApp/service/updatedevaluator/parser/ParserImpl.java
@@ -19,62 +19,50 @@ public class ParserImpl implements Parser {
         return lexemes.stream().filter(token -> token instanceof NumberToken).toList().size();
     }
 
-
-//    private Double evaluateTokens(LinkedList<Token> tokens) {
-//        if (tokens.isEmpty()) throw new RuntimeException("empty!");
-//        var nextToken = tokens.removeFirst();
-//        if (nextToken instanceof NumberToken) {
-//            var leftOperand = ((NumberToken) nextToken).getNumber();
-//            if (tokens.isEmpty() || (tokens.size() == 1 && tokens.getFirst() instanceof BracketToken && !((BracketToken) tokens.getFirst()).isOpen())) return leftOperand;
-//            var operation = tokens.removeFirst();
-//            if (operation instanceof OperationToken) {
-//                var rightOperand = evaluateTokens(tokens);
-//                return ((OperationToken) operation).doOperation(leftOperand, rightOperand);
-//            } else throw new RuntimeException("operation expected!");
-//        } else if (nextToken instanceof BracketToken) {
-//            if (!((BracketToken) nextToken).isOpen()) throw new MissedLeftBracketException();
-//            var result = evaluateTokens(tokens);
-//            if (tokens.isEmpty()) throw new MissedRightBracketException();
-//            var lastToken = tokens.removeFirst();
-//            if (!(lastToken instanceof BracketToken) || ((BracketToken) lastToken).isOpen())
-//                throw new RuntimeException();
-//            return result;
-//        } else throw new RuntimeException("unexpected token!");
-//
-//    }
     private Double customEvaluateTokens(LinkedList<Token> tokens) {
         if (tokens.isEmpty()) throw new RuntimeException("empty!");
         var nextToken = tokens.removeFirst();
         if (nextToken instanceof NumberToken) {
             var leftOperand = ((NumberToken) nextToken).getNumber();
-            if (tokens.isEmpty() || (tokens.size() == 1 && tokens.getFirst() instanceof BracketToken && !((BracketToken) tokens.getFirst()).isOpen())) return leftOperand;
+            if (expressionIsEnded(tokens)) return leftOperand;
             var operation = tokens.removeFirst();
-            if(operation instanceof OperationToken) {
-                if(((OperationToken) operation).getOperationType().equals(OperationType.MULTIPLICATION) || ((OperationToken) operation).getOperationType().equals(OperationType.DIVISION)){
-                    var rightToken = tokens.getFirst();
-                    var res = 0.0;
-                    if(rightToken instanceof NumberToken) {
-                        res = ((OperationToken) operation).doOperation(leftOperand, ((NumberToken) rightToken).getNumber());
-                        tokens.removeFirst();
-
-                    } else if (rightToken instanceof BracketToken && ((BracketToken) rightToken).isOpen()) {
-                        res = leftOperand * customEvaluateTokens(tokens);
-                    }
-                    tokens.addFirst(new NumberToken(res));
-                    return customEvaluateTokens(tokens);
-                } else {
-                    return ((OperationToken) operation).doOperation(leftOperand, customEvaluateTokens(tokens));
-                }
+            if (operation instanceof OperationToken) {
+                var operationType = ((OperationToken) operation).getOperationType();
+                return operationType.equals(OperationType.MULTIPLICATION) || operationType.equals(OperationType.DIVISION) ?
+                        getMultiplyOrDivideResult(tokens, leftOperand, (OperationToken) operation) :
+                        ((OperationToken) operation).doOperation(leftOperand, customEvaluateTokens(tokens));
             } else throw new RuntimeException("operation expected!");
         } else if (nextToken instanceof BracketToken) {
-            if (!((BracketToken) nextToken).isOpen()) throw new MissedLeftBracketException();
-            var result = customEvaluateTokens(tokens);
-            if (tokens.isEmpty()) throw new MissedRightBracketException();
-            var lastToken = tokens.removeFirst();
-            if (!(lastToken instanceof BracketToken) || ((BracketToken) lastToken).isOpen())
-                throw new RuntimeException();
-            return result;
+            return getResultFromBrackets(tokens, (BracketToken) nextToken);
         } else throw new RuntimeException("unexpected token!");
+    }
+
+    private boolean expressionIsEnded(LinkedList<Token> tokens) {
+        return tokens.isEmpty() || (tokens.size() == 1 && tokens.getFirst() instanceof BracketToken && !((BracketToken) tokens.getFirst()).isOpen());
+    }
+
+    private Double getMultiplyOrDivideResult(LinkedList<Token> tokens, Double leftOperand, OperationToken operation) {
+        var rightToken = tokens.getFirst();
+        var res = 0.0;
+        if (rightToken instanceof NumberToken) {
+            res = operation.doOperation(leftOperand, ((NumberToken) rightToken).getNumber());
+            tokens.removeFirst();
+
+        } else if (rightToken instanceof BracketToken && ((BracketToken) rightToken).isOpen()) {
+            res = leftOperand * customEvaluateTokens(tokens);
+        } else throw new BadOperandException();
+        tokens.addFirst(new NumberToken(res));
+        return customEvaluateTokens(tokens);
+    }
+
+    private Double getResultFromBrackets(LinkedList<Token> tokens, BracketToken nextToken) {
+        if (!nextToken.isOpen()) throw new MissedLeftBracketException();
+        var result = customEvaluateTokens(tokens);
+        if (tokens.isEmpty()) throw new MissedRightBracketException();
+        var lastToken = tokens.removeFirst();
+        if (!(lastToken instanceof BracketToken) || ((BracketToken) lastToken).isOpen())
+            throw new RuntimeException();
+        return result;
     }
 
 }
