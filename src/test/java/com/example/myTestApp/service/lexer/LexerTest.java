0ea3eeb742ea2ada961ad4e1ca9c553f700c6e7e
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-04 01:09:46 +0200

    refactored tests

--- src/test/java/com/example/myTestApp/service/lexer/LexerTest.java
+++ src/test/java/com/example/myTestApp/service/lexer/LexerTest.java
@@ -4,6 +4,8 @@ import com.example.myTestApp.service.lexer.Lexer;
 import com.example.myTestApp.service.lexer.LexerImpl;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class LexerTest {
     private final Lexer lexer = new LexerImpl();
 
@@ -11,14 +13,14 @@ class LexerTest {
     void toLexemeList() {
         var expression = "2.5+44/(2--2)+ 3.5";
         var res = lexer.tokenize(expression);
-        System.out.println(res);
+        assertEquals(res.toString(), "[2.5, ADDITION, 44.0, DIVISION, (, 2.0, SUBTRACTION, -2.0, ), ADDITION, 3.5]");
 
         expression = "2+3+(2+((2+2))) -6";
         res = lexer.tokenize(expression);
-        System.out.println(res);
+        assertEquals(res.toString(), "[2.0, ADDITION, 3.0, ADDITION, (, 2.0, ADDITION, (, (, 2.0, ADDITION, 2.0, ), ), ), SUBTRACTION, 6.0]");
 
         expression = "2++2 -+2+-2";
         res = lexer.tokenize(expression);
-        System.out.println(res);
+        assertEquals(res.toString(), "[2.0, ADDITION, 2.0, SUBTRACTION, 2.0, ADDITION, -2.0]");
     }
 }
